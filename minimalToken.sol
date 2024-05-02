36aaecfd697180248665691b89b1b9f8297e7d07
Aditya Ojha <58110004+codingbot247@users.noreply.github.com>
2024-05-03 04:04:34 +0530

    Update minimalToken.sol
    Corrected the code and removed any issues

--- minimalToken.sol
+++ minimalToken.sol
@@ -1,3 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Mytoken is ERC20, Ownable(msg.sender) {
+    constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) payable {
+        _mint(msg.sender, initialSupply);
+    }
+    
+    function releaseToken(address _to, uint256 _amount) public onlyOwner {
+        _transfer(owner(), _to, _amount);
+    }
+
+    function transfer(address _to, uint256 _amount) public override returns (bool){
+    require(balanceOf(msg.sender) >= _amount, "Insufficient balance bro");
+    _transfer(msg.sender, _to, _amount);    
+    //transfer function takes care of updating the balance too
+    return true; 
+    
+ }
+
+}
+
+
+
+
+
+
+
+
+
+/*
 //SPDX-LICENSE-IDENTIFIER: MIT
 pragma solidity ^0.8.7;
 
@@ -18,4 +52,6 @@ require(balance[msg.sender] >= _amount, "Insufficient balance bro");
 balance[msg.sender] = balance[msg.sender]- _amount;
 balance[_to] = balance[_to] + _amount;
 }
-}
+} */
+
+
